6ffe020b436d6ca203c9c13db7cb093532adbf30
Altrios <famartinez.ti@gmail.com>
2026-02-03 09:51:11 +0100

    parametros a metodos

--- clases.js
+++ clases.js
@@ -1,7 +1,16 @@
-export class Telefono{
-    constructor(){}//solo se usa constructor si la clase va a recibir parametros ya que este es quien los recibe e interactua con ellos y los hace accesible para el resto de la clase
-    peso="10kg";
-    llamada(){
-        console.log(this.peso)
-    }
+export class Telefono {
+  constructor() {} //solo se usa constructor si la clase va a recibir parametros ya que este es quien los recibe e interactua con ellos y los hace accesible para el resto de la clase
+  peso = "10kg";
+  llamada() {
+    console.log(this.peso);
+  }
+}
+
+export class Parametro {
+  constructor() {}
+
+//   los metodos pueden recibir parametros como si se tratase de una funcion comun y corriente
+  suma(a, b) {
+    console.log(a + b);
+  }
 }
--- modulo.js
+++ modulo.js
@@ -1,4 +1,4 @@
-import { Telefono } from "./clases.js";
+import { Telefono, Parametro } from "./clases.js";
 
 // con esto creamos un nuevo objeto de tipo "telefono" el cual previamente lo creamos desde clases.js, este no representa directamente el objeto, mas bien una copia
 const objeto= new Telefono();
@@ -6,3 +6,10 @@ const objeto= new Telefono();
 // el console.log que tiene "llamada" se ejecuta por si solo al llamar al objeto y luego como a un json ir directo al metodo como si se tratase de una propiedad (porque lo es)
 objeto.llamada();
 console.log(objeto.peso)
+
+// //////////////////////////////
+
+const suma=new Parametro();
+
+// para enviar un parametroa  un metodo, primero se debe llegar a el a traves del objeto como si se tratase de un json y luego enviarle los paramtros de la misma manera que se hace con una funcion
+suma.suma(4, 5)
